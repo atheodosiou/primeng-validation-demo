2a6691307ca52d4813ba97f468108ba80cc54e67
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-09-18 22:48:53 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { SelectItem, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessageService]
 })
-export class AppComponent {
-  title = 'primeng-validation';
+export class AppComponent implements OnInit {
+  userform: FormGroup;
+
+  submitted: boolean;
+
+  genders: SelectItem[];
+
+  description: string;
+
+  constructor(private fb: FormBuilder, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.userform = this.fb.group({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      description: new FormControl(''),
+      gender: new FormControl('', Validators.required)
+    });
+
+    this.genders = [];
+    this.genders.push({ label: 'Select Gender', value: '' });
+    this.genders.push({ label: 'Male', value: 'Male' });
+    this.genders.push({ label: 'Female', value: 'Female' });
+  }
+
+  onSubmit(value: string) {
+    this.submitted = true;
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'Form Submitted', sticky: true });
+  }
+
+  get diagnostic() { return JSON.stringify(this.userform.value); }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { MessageModule } from 'primeng/message';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
+import { PanelModule } from 'primeng/panel';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MessageModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastModule,
+    PanelModule,
+    DropdownModule,
+    InputTextModule,
+    InputTextareaModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
